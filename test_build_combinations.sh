f36e311845b5024f459a0db61c8a83111b831ff1
hardliner66 <hardliner66@gmail.com>
2023-10-17 22:31:44 +0200

    use proper config files for build configs

--- test_build_combinations.sh
+++ test_build_combinations.sh
@@ -4,4 +4,4 @@ cargo check -F single_thread
 
 cargo check -F wasm --target wasm32-unknown-unknown
 
-RUSTFLAGS=--cfg=web_sys_unstable_apis cargo check -F wasm --target wasm32-unknown-unknown
+RUSTFLAGS= cargo check -F wasm --target wasm32-unknown-unknown
