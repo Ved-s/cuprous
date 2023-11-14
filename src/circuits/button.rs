16ee39cb7999e4c5fca885c47a9ed7467fcca51a
Ved-s <vedomir.samarin.03@gmail.com>
2023-11-15 00:11:35 +1100

    maximum button customizeability

--- src/circuits/button.rs
+++ src/circuits/button.rs
@@ -1,26 +1,45 @@
 use eframe::{
-    egui::{PointerButton, Sense, CursorIcon},
-    epaint::{Color32, FontId, Rounding},
+    egui::{CursorIcon, PointerButton, Sense},
+    epaint::{Color32, FontId, Rounding, Stroke},
 };
-use emath::{Align2, pos2, vec2};
+use emath::{pos2, vec2, Align2};
 
-use crate::{Direction4, describe_directional_circuit, unwrap_option_or_return};
+use crate::{unwrap_option_or_return, Direction4};
 
-use super::{*, props::CircuitProperty};
+use super::{
+    props::{CircuitProperty, RangedValue},
+    *,
+};
+
+struct ButtonVisuals {
+    color: Color32,
+    stroke: Stroke,
+    font_color: Color32,
+    font_scale: f32,
+    rounding: Rounding,
+}
 
 struct Button {
     out_pin: CircuitPinInfo,
-    color: Color32,
-    text: Arc<str>
+    text: Arc<str>,
+    size: Vec2u,
+    visuals: ButtonVisuals,
 }
 
 impl Button {
     fn new() -> Self {
-        let description = Self::describe(Direction4::Right);
+        let description = Self::describe(Direction4::Right, 3, 3);
         Self {
             out_pin: description.pins[0].to_info(),
-            color: Color32::from_rgb(200, 30, 30),
-            text: "PUSH".into()
+            text: "PUSH".into(),
+            size: 3.into(),
+            visuals: ButtonVisuals {
+                color: Color32::from_rgb(200, 30, 30),
+                font_color: Color32::WHITE,
+                rounding: Rounding::same(0.75),
+                font_scale: 0.5,
+                stroke: Stroke::NONE
+            },
         }
     }
 
@@ -33,7 +52,13 @@ impl Button {
         );
     }
 
-    fn draw_button(state: Option<&CircuitStateContext>, color: Color32, text: &str, ctx: &PaintContext, semi_transparent: bool) {
+    fn draw_button(
+        state: Option<&CircuitStateContext>,
+        text: &str,
+        visuals: &ButtonVisuals,
+        ctx: &PaintContext,
+        semi_transparent: bool,
+    ) {
         let color_mul = if semi_transparent { 0.5 } else { 1.0 };
         let state = state
             .map(|s| {
@@ -42,38 +67,67 @@ impl Button {
             })
             .unwrap_or_default();
         let color = if state {
-            let c = color.linear_multiply(0.77);
-            Color32::from_rgba_premultiplied(c.r(), c.g(), c.b(), color.a())
+            let c = visuals.color.linear_multiply(0.77);
+            Color32::from_rgba_premultiplied(c.r(), c.g(), c.b(), visuals.color.a())
         } else {
-            color
+            visuals.color
         }
         .linear_multiply(color_mul);
-        ctx.paint
-            .circle_filled(ctx.rect.center(), ctx.rect.width() * 0.5, color);
 
-        let font = FontId::monospace(ctx.screen.scale * 0.5);
+        let rounding = Rounding {
+            nw: visuals.rounding.nw * ctx.screen.scale,
+            ne: visuals.rounding.ne * ctx.screen.scale,
+            sw: visuals.rounding.sw * ctx.screen.scale,
+            se: visuals.rounding.se * ctx.screen.scale,
+        };
+
+        let stroke = Stroke {
+            width: visuals.stroke.width * ctx.screen.scale,
+            color: visuals.stroke.color,
+        };
+
+        ctx.paint.rect(ctx.rect, rounding, color, stroke);
+
+        let font = FontId::monospace(ctx.screen.scale * visuals.font_scale);
 
         ctx.paint.text(
             ctx.rect.center(),
             Align2::CENTER_CENTER,
             text,
             font,
-            Color32::WHITE,
+            visuals.font_color,
         );
     }
 
     fn describe_props(props: &CircuitPropertyStore) -> CircuitDescription<1> {
         let dir = props.read_clone("dir").unwrap_or(Direction4::Right);
-        Self::describe(dir)
+        let width = props
+            .read("width", |v: &RangedValue<u32>| v.get())
+            .unwrap_or(3);
+        let height = props
+            .read("height", |v: &RangedValue<u32>| v.get())
+            .unwrap_or(3);
+        Self::describe(dir, width, height)
     }
 
-    fn describe(dir: Direction4) -> CircuitDescription<1> {
-        describe_directional_circuit! {
-            default_dir: Right,
-            dir: dir,
-            size: [3, 3],
+    fn describe(dir: Direction4, width: u32, height: u32) -> CircuitDescription<1> {
+        let pin_pos = match dir {
+            Direction4::Up => [width / 2, 0],
+            Direction4::Left => [0, height / 2],
+            Direction4::Down => [width / 2, height - 1],
+            Direction4::Right => [width - 1, height / 2],
+        };
 
-            "out": Outside, "Out", Right, [2, 1]
+        crate::circuits::CircuitDescription {
+            size: [width, height].into(),
+            pins: [crate::circuits::CircuitPinDescription {
+                active: true,
+                name: "out".into(),
+                dir: InternalPinDirection::Outside,
+                display_name: "Out".into(),
+                display_dir: Some(dir),
+                pos: pin_pos.into(),
+            }],
         }
     }
 }
@@ -88,29 +142,50 @@ impl CircuitImpl for Button {
     }
 
     fn control_info(&self, circuit: &Arc<Circuit>, id: usize) -> Option<CircuitControlInfo> {
+        let width = circuit
+            .props
+            .read("width", |v: &RangedValue<u32>| v.get())
+            .unwrap_or(3);
+        let height = circuit
+            .props
+            .read("height", |v: &RangedValue<u32>| v.get())
+            .unwrap_or(3);
+
         match id {
             0 => Some(CircuitControlInfo {
-                rect: Rect::from_min_size(pos2(0.75, 0.75), vec2(1.5, 1.5)),
-                display_name: circuit.name().map(|arc| arc.into()).unwrap_or_else(|| "Button".into()),
+                rect: Rect::from_min_size(
+                    pos2(0.75, 0.75),
+                    vec2(width as f32 - 1.5, height as f32 - 1.5),
+                ),
+                display_name: circuit
+                    .name()
+                    .map(|arc| arc.into())
+                    .unwrap_or_else(|| "Button".into()),
             }),
             _ => None,
         }
     }
 
     fn update_control(
-            &self,
-            id: usize,
-            _: &Arc<Circuit>,
-            state: Option<&CircuitStateContext>,
-            ctx: &PaintContext,
-            interactive: bool,
-            uid: Id,
-        ) {
+        &self,
+        id: usize,
+        _: &Arc<Circuit>,
+        state: Option<&CircuitStateContext>,
+        ctx: &PaintContext,
+        interactive: bool,
+        uid: Id,
+    ) {
         if id != 0 {
             return;
         }
 
-        Self::draw_button(state, self.color, &self.text, ctx, false);
+        Self::draw_button(
+            state,
+            &self.text,
+            &self.visuals,
+            ctx,
+            false,
+        );
         if !interactive {
             return;
         }
@@ -119,11 +194,7 @@ impl CircuitImpl for Button {
 
         let id = ctx.ui.auto_id_with(uid);
 
-        let interaction = ctx.ui.interact(
-            ctx.rect,
-            id,
-            Sense::drag(),
-        );
+        let interaction = ctx.ui.interact(ctx.rect, id, Sense::drag());
         if interaction.hovered() {
             ctx.ui.ctx().set_cursor_icon(CursorIcon::PointingHand);
         }
@@ -139,21 +210,58 @@ impl CircuitImpl for Button {
         }
     }
 
-    fn prop_changed(&self, prop_id: &str, _: &mut bool, recreate_pins: &mut bool) {
+    fn prop_changed(&self, prop_id: &str, resize: &mut bool, recreate_pins: &mut bool) {
         if prop_id == "dir" {
-            *recreate_pins = true
+            *recreate_pins = true;
+        }
+        if prop_id == "width" || prop_id == "height" {
+            *recreate_pins = true;
+            *resize = true;
         }
     }
 
     fn apply_props(&mut self, circ: &Arc<Circuit>, changed: Option<&str>) {
-        if matches!(changed, None | Some("btn_color")) {
-            if let Some(color) = circ.props.read_clone("btn_color") {
-                self.color = color;
+        if matches!(changed, None | Some("color")) {
+            if let Some(color) = circ.props.read_clone("color") {
+                self.visuals.color = color;
             }
         }
-        if matches!(changed, None | Some("btn_text")) {
-            if let Some(text) = circ.props.read("btn_text", |s: &ArcString| s.get_arc()) {
+        if matches!(changed, None | Some("stroke")) {
+            if let Some(stroke) = circ.props.read_clone("stroke") {
+                self.visuals.stroke = stroke;
+            }
+        }
+        if matches!(changed, None | Some("font_color")) {
+            if let Some(font_color) = circ.props.read_clone("font_color") {
+                self.visuals.font_color = font_color;
+            }
+        }
+        if matches!(changed, None | Some("text")) {
+            if let Some(text) = circ.props.read("text", |s: &ArcString| s.get_arc()) {
                 self.text = text;
+            }
+        }
+        if matches!(changed, None | Some("width")) {
+            if let Some(value) = circ.props.read("width", |v: &RangedValue<u32>| v.get()) {
+                *self.size.x_mut() = value;
+            }
+        }
+        if matches!(changed, None | Some("height")) {
+            if let Some(value) = circ.props.read("height", |v: &RangedValue<u32>| v.get()) {
+                *self.size.y_mut() = value;
+            }
+        }
+        if matches!(changed, None | Some("font_scale")) {
+            if let Some(value) = circ
+                .props
+                .read("font_scale", |v: &RangedValue<f32>| v.get())
+            {
+                self.visuals.font_scale = value;
+            }
+        }
+        if matches!(changed, None | Some("rounding")) {
+            if let Some(value) = circ.props.read_clone("rounding") {
+                self.visuals.rounding = value;
             }
         }
     }
@@ -176,7 +284,7 @@ impl CircuitImpl for Button {
         &self,
         _: &CircuitStateContext,
         data: &serde_intermediate::Intermediate,
-        _: bool
+        _: bool,
     ) -> Option<Box<dyn InternalCircuitState>> {
         serde_intermediate::de::intermediate::deserialize::<ButtonState>(data)
             .ok()
@@ -206,11 +314,34 @@ impl CircuitPreviewImpl for ButtonPreview {
     fn draw_preview(&self, props: &CircuitPropertyStore, ctx: &PaintContext, in_world: bool) {
         Button::draw_base(ctx, in_world);
         let button_ctx = ctx.with_rect(ctx.rect.shrink(ctx.screen.scale * 0.75));
-        let color = props.read_clone("btn_color").unwrap_or(Color32::from_rgb(200, 30, 30));
-        let text = props.read("btn_text", |s: &ArcString| s.get_arc());
+        let color = props
+            .read_clone("color")
+            .unwrap_or(Color32::from_rgb(200, 30, 30));
+        let text = props.read("text", |s: &ArcString| s.get_arc());
         let text = text.as_deref().unwrap_or("PUSH");
 
-        Button::draw_button(None, color, text, &button_ctx, in_world);
+        let font_scale = props
+            .read("font_scale", |v: &RangedValue<f32>| v.get())
+            .unwrap_or(0.5);
+        let rounding = props.read_clone("rounding").unwrap_or(Rounding::same(0.75));
+        let font_color = props.read_clone("font_color").unwrap_or(Color32::WHITE);
+        let stroke = props.read_clone("stroke").unwrap_or(Stroke::NONE);
+
+        let visuals = ButtonVisuals {
+            color,
+            stroke,
+            font_color,
+            font_scale,
+            rounding,
+        };
+
+        Button::draw_button(
+            None,
+            text,
+            &visuals,
+            &button_ctx,
+            in_world,
+        );
     }
 
     fn create_impl(&self) -> Box<dyn CircuitImpl> {
@@ -225,7 +356,7 @@ impl CircuitPreviewImpl for ButtonPreview {
         &self,
         _: &serde_intermediate::Intermediate,
         _: &serde_intermediate::Intermediate,
-        _: &Arc<SimulationContext>
+        _: &Arc<SimulationContext>,
     ) -> Option<Box<dyn CircuitPreviewImpl>> {
         Some(Box::new(ButtonPreview {}))
     }
@@ -233,8 +364,18 @@ impl CircuitPreviewImpl for ButtonPreview {
     fn default_props(&self) -> CircuitPropertyStore {
         CircuitPropertyStore::new([
             CircuitProperty::new("dir", "Direction", Direction4::Right),
-            CircuitProperty::new("btn_color", "Color", Color32::from_rgb(200, 30, 30)),
-            CircuitProperty::new("btn_text", "Text", ArcString::from("PUSH")),
+            CircuitProperty::new("color", "Color", Color32::from_rgb(200, 30, 30)),
+            CircuitProperty::new("stroke", "Stroke", Stroke::NONE),
+            CircuitProperty::new("font_color", "Font color", Color32::WHITE),
+            CircuitProperty::new("text", "Text", ArcString::from("PUSH")),
+            CircuitProperty::new("width", "Width", RangedValue::new(3..=u32::MAX, 1, 3)),
+            CircuitProperty::new("height", "Height", RangedValue::new(3..=u32::MAX, 1, 3)),
+            CircuitProperty::new(
+                "font_scale",
+                "Font scale",
+                RangedValue::new(0.01..=f32::MAX, 0.05, 0.5),
+            ),
+            CircuitProperty::new("rounding", "Rounding", Rounding::same(0.75)),
         ])
     }
 
--- src/circuits/props.rs
+++ src/circuits/props.rs
@@ -1,11 +1,17 @@
 use std::{
     any::{Any, TypeId},
-    collections::HashMap, ops::Deref,
+    collections::HashMap,
+    ops::{Deref, RangeInclusive},
 };
 
-use eframe::{egui::{Ui, ComboBox}, epaint::Color32};
+use eframe::{
+    egui::{ComboBox, DragValue, Ui, Widget, self},
+    epaint::{Color32, Rounding, Stroke},
+};
 
-use crate::{unwrap_option_or_return, Direction4, DynStaticStr, RwLock, unwrap_option_or_continue, ArcString};
+use crate::{
+    unwrap_option_or_continue, unwrap_option_or_return, ArcString, Direction4, DynStaticStr, RwLock,
+};
 
 pub struct CircuitPropertyStore(RwLock<HashMap<DynStaticStr, CircuitProperty>>);
 
@@ -68,17 +74,11 @@ impl CircuitPropertyStore {
 
     pub fn save(&self) -> crate::io::CircuitPropertyStoreData {
         crate::io::CircuitPropertyStoreData(HashMap::from_iter(
-            self.0
-                .read()
-                .values()
-                .map(|p| (p.id.clone(), p.imp.save())),
+            self.0.read().values().map(|p| (p.id.clone(), p.imp.save())),
         ))
     }
 
-    pub fn load(
-        &self,
-        data: &crate::io::CircuitPropertyStoreData
-    ) {
+    pub fn load(&self, data: &crate::io::CircuitPropertyStoreData) {
         let mut lock = self.0.write();
         for (id, data) in data.0.iter() {
             let prop = lock.get_mut(id);
@@ -107,18 +107,19 @@ impl CircuitPropertyStore {
         [
             CircuitProperty::new("name", "Name", ArcString::default()),
             CircuitProperty::new("label_dir", "Label dir", Direction4::Down),
-        ].into_iter()
+        ]
+        .into_iter()
     }
 }
 
 impl Clone for CircuitPropertyStore {
     fn clone(&self) -> Self {
-        Self(RwLock::new(HashMap::from_iter(
-            self.0.read().values().map(|p| {
+        Self(RwLock::new(HashMap::from_iter(self.0.read().values().map(
+            |p| {
                 let clone = p.clone();
                 (clone.id(), clone)
-            }),
-        )))
+            },
+        ))))
     }
 }
 
@@ -135,7 +136,6 @@ pub trait CircuitPropertyImpl: Any + Send + Sync {
 }
 
 impl dyn CircuitPropertyImpl {
-
     #[inline]
     pub fn is<T: CircuitPropertyImpl>(&self) -> bool {
         let t = TypeId::of::<T>();
@@ -181,15 +181,23 @@ impl dyn CircuitPropertyImpl {
 pub struct CircuitProperty {
     imp: Box<dyn CircuitPropertyImpl>,
     id: DynStaticStr,
-    name: DynStaticStr
+    name: DynStaticStr,
 }
 
 impl CircuitProperty {
-    pub fn new<T: CircuitPropertyImpl>(id: impl Into<DynStaticStr>, name: impl Into<DynStaticStr>, prop: T) -> Self {
+    pub fn new<T: CircuitPropertyImpl>(
+        id: impl Into<DynStaticStr>,
+        name: impl Into<DynStaticStr>,
+        prop: T,
+    ) -> Self {
         Self::new_dynamic(id, name, Box::new(prop))
     }
 
-    pub fn new_dynamic(id: impl Into<DynStaticStr>, name: impl Into<DynStaticStr>, prop: Box<dyn CircuitPropertyImpl>) -> Self {
+    pub fn new_dynamic(
+        id: impl Into<DynStaticStr>,
+        name: impl Into<DynStaticStr>,
+        prop: Box<dyn CircuitPropertyImpl>,
+    ) -> Self {
         Self {
             imp: prop,
             id: id.into(),
@@ -197,7 +205,10 @@ impl CircuitProperty {
         }
     }
 
-    pub fn new_default<T: CircuitPropertyImpl + Default>(id: impl Into<DynStaticStr>, name: impl Into<DynStaticStr>) -> Self {
+    pub fn new_default<T: CircuitPropertyImpl + Default>(
+        id: impl Into<DynStaticStr>,
+        name: impl Into<DynStaticStr>,
+    ) -> Self {
         Self::new(id, name, T::default())
     }
 
@@ -220,7 +231,48 @@ impl CircuitProperty {
 
 impl Clone for CircuitProperty {
     fn clone(&self) -> Self {
-        Self { imp: self.imp.clone(), id: self.id.clone(), name: self.name.clone() }
+        Self {
+            imp: self.imp.clone(),
+            id: self.id.clone(),
+            name: self.name.clone(),
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct RangedValue<T>
+where
+    T: emath::Numeric + Send + Sync,
+{
+    range: RangeInclusive<T>,
+    change_speed: T,
+    value: T,
+}
+
+impl<T> RangedValue<T>
+where
+    T: emath::Numeric + Send + Sync,
+{
+    pub fn new(range: RangeInclusive<T>, change_speed: T, value: T) -> Self {
+        Self {
+            range,
+            change_speed,
+            value,
+        }
+    }
+
+    pub fn get(&self) -> T {
+        self.value
+    }
+
+    pub fn set(&mut self, value: T) {
+        if value < *self.range.start() {
+            self.value = *self.range.start()
+        }
+        if value > *self.range.end() {
+            self.value = *self.range.end()
+        }
+        self.value = value;
     }
 }
 
@@ -234,15 +286,19 @@ impl CircuitPropertyImpl for Direction4 {
         let mut changed = false;
         ui.skip_ahead_auto_ids(1);
         ComboBox::from_id_source(ui.next_auto_id())
-        .selected_text(if not_equal { Default::default() } else { self.name() })
-        .show_ui(ui, |ui| {
-            for dir in Direction4::iter_all() {
-                let res = ui.selectable_value(self, dir, dir.name());
-                if res.changed() || res.clicked() {
-                    changed = true;
+            .selected_text(if not_equal {
+                Default::default()
+            } else {
+                self.name()
+            })
+            .show_ui(ui, |ui| {
+                for dir in Direction4::iter_all() {
+                    let res = ui.selectable_value(self, dir, dir.name());
+                    if res.changed() || res.clicked() {
+                        changed = true;
+                    }
                 }
-            }
-        });
+            });
         changed.then(|| Box::new(old) as Box<dyn CircuitPropertyImpl>)
     }
 
@@ -273,7 +329,6 @@ impl CircuitPropertyImpl for bool {
     }
 
     fn ui(&mut self, ui: &mut Ui, not_equal: bool) -> Option<Box<dyn CircuitPropertyImpl>> {
-
         let text = if not_equal {
             "<many>"
         } else if *self {
@@ -329,15 +384,13 @@ impl CircuitPropertyImpl for ArcString {
         };
 
         if ui.text_edit_singleline(r).changed() {
-
             if not_equal {
                 let string = self.get_mut();
                 string.clear();
                 string.push_str(&empty);
             }
             Some(Box::new(ArcString::from(old.deref())))
-        }
-        else {
+        } else {
             None
         }
     }
@@ -369,7 +422,7 @@ impl CircuitPropertyImpl for ArcString {
 
 impl CircuitPropertyImpl for Color32 {
     fn equals(&self, other: &dyn CircuitPropertyImpl) -> bool {
-        other.is_type_and(|s: &Color32| *s == *self)
+        other.is_type_and(|s: &Self| *s == *self)
     }
 
     fn ui(&mut self, ui: &mut Ui, not_equal: bool) -> Option<Box<dyn CircuitPropertyImpl>> {
@@ -380,8 +433,7 @@ impl CircuitPropertyImpl for Color32 {
         if ui.color_edit_button_srgba(&mut color).changed() {
             *self = color;
             Some(Box::new(old))
-        }
-        else {
+        } else {
             None
         }
     }
@@ -402,6 +454,134 @@ impl CircuitPropertyImpl for Color32 {
 
     fn copy_into(&self, other: &mut dyn CircuitPropertyImpl) {
         if let Some(r) = other.downcast_mut::<Self>() {
+            *r = *self;
+        }
+    }
+}
+
+impl<T> CircuitPropertyImpl for RangedValue<T>
+where
+    T: emath::Numeric + Send + Sync
+{
+    fn equals(&self, other: &dyn CircuitPropertyImpl) -> bool {
+        other.is_type_and(|s: &Self| s.value == self.value)
+    }
+
+    fn ui(&mut self, ui: &mut Ui, not_equal: bool) -> Option<Box<dyn CircuitPropertyImpl>> {
+        let old = Clone::clone(self);
+
+        let mut value = if not_equal { *self.range.start() } else { self.value };
+
+        let resp = DragValue::new(&mut value)
+            .clamp_range(self.range.clone())
+            .speed(self.change_speed.to_f64())
+            .ui(ui);
+
+        if resp.changed() {
+            self.set(value);
+            Some(Box::new(old))
+        }
+        else {
+            None
+        }
+    }
+
+    fn clone(&self) -> Box<dyn CircuitPropertyImpl> {
+        Box::new(Clone::clone(self))
+    }
+
+    fn load(&mut self, data: &serde_intermediate::Intermediate) {
+        if let Ok(value) = serde_intermediate::from_intermediate(data) {
+            self.set(T::from_f64(value));
+        }
+    }
+
+    fn save(&self) -> serde_intermediate::Intermediate {
+        serde_intermediate::to_intermediate(&self.value.to_f64()).unwrap_or_default()
+    }
+
+    fn copy_into(&self, other: &mut dyn CircuitPropertyImpl) {
+        if let Some(r) = other.downcast_mut::<Self>() {
+            r.set(self.value);
+        }
+    }
+}
+
+impl CircuitPropertyImpl for Rounding {
+    fn equals(&self, other: &dyn CircuitPropertyImpl) -> bool {
+        other.is_type_and(|o: &Self| o == self)
+    }
+
+    fn ui(&mut self, ui: &mut Ui, _: bool) -> Option<Box<dyn CircuitPropertyImpl>> {
+        let old = *self;
+        let changed = egui::Grid::new(ui.next_auto_id().with("rounding_grid")).show(ui, |ui| {
+            let nw = DragValue::new(&mut self.nw).clamp_range(0.0 ..= f32::MAX).speed(0.05).ui(ui).changed();
+            let ne = DragValue::new(&mut self.ne).clamp_range(0.0 ..= f32::MAX).speed(0.05).ui(ui).changed();
+            ui.end_row();
+            let sw = DragValue::new(&mut self.sw).clamp_range(0.0 ..= f32::MAX).speed(0.05).ui(ui).changed();
+            let se = DragValue::new(&mut self.se).clamp_range(0.0 ..= f32::MAX).speed(0.05).ui(ui).changed();
+            ui.end_row();
+
+            nw || ne || sw || se
+        });
+
+        changed.inner.then(|| Box::new(old) as Box<dyn CircuitPropertyImpl>)
+    }
+
+    fn clone(&self) -> Box<dyn CircuitPropertyImpl> {
+        Box::new(Clone::clone(self))
+    }
+
+    fn save(&self) -> serde_intermediate::Intermediate {
+        serde_intermediate::to_intermediate(&self).unwrap_or_default()
+    }
+
+    fn load(&mut self, data: &serde_intermediate::Intermediate) {
+        if let Ok(d) = serde_intermediate::de::intermediate::deserialize(data) {
+            *self = d;
+        }
+    }
+
+    fn copy_into(&self, other: &mut dyn CircuitPropertyImpl) {
+        if let Some(r) = other.downcast_mut() {
+            *r = *self;
+        }
+    }
+}
+
+impl CircuitPropertyImpl for Stroke {
+    fn equals(&self, other: &dyn CircuitPropertyImpl) -> bool {
+        other.is_type_and(|o: &Self| o == self)
+    }
+
+    fn ui(&mut self, ui: &mut Ui, _: bool) -> Option<Box<dyn CircuitPropertyImpl>> {
+        let old = *self;
+        let changed = ui.horizontal(|ui| {
+            let width = DragValue::new(&mut self.width).clamp_range(0.0 ..= f32::MAX).speed(0.05).ui(ui).changed();
+            let color = ui.color_edit_button_srgba(&mut self.color).changed();
+
+            width || color
+        });
+
+        changed.inner.then(|| Box::new(old) as Box<dyn CircuitPropertyImpl>)
+    }
+
+    fn clone(&self) -> Box<dyn CircuitPropertyImpl> {
+        Box::new(Clone::clone(self))
+    }
+
+    fn save(&self) -> serde_intermediate::Intermediate {
+        serde_intermediate::to_intermediate(&self).unwrap_or_default()
+    }
+
+    fn load(&mut self, data: &serde_intermediate::Intermediate) {
+        if let Ok(d) = serde_intermediate::de::intermediate::deserialize(data) {
+            *self = d;
+        }
+    }
+
+    fn copy_into(&self, other: &mut dyn CircuitPropertyImpl) {
+        if let Some(r) = other.downcast_mut() {
             *r = *self;
         }
     }
